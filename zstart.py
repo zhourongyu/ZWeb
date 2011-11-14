0fbc5402afc06103e55e4d71b6bcad8fd257582c
Rony <zuroyu@gmail.com>
2011-11-14 23:40:47 +0800

    tornado

--- zstart.py
+++ zstart.py
@@ -1,46 +1,85 @@
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp.util import run_wsgi_app
-from google.appengine.ext.webapp import template
 import os.path
+import tornado.options
+import tornado.web
+import tornado.httpserver
+import tornado.database
 
-class MainPage(webapp.RequestHandler):
-    
+from tornado.options import define, options
+define("port", default=8888, help="run on the given port", type=int)
+define("mysql_host", default="127.0.0.1:3306", help="blog database host")
+define("mysql_database", default="blog", help="blog database name")
+define("mysql_user", default="root", help="blog database user")
+define("mysql_password", default="123", help="blog database password")
+
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", HomeHandler),
+            (r"/notes", NotesHandler),
+            (r"/site", SiteHandler),
+   
+        ]
+        settings = dict(
+            blog_title=u"Tornado Blog",
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            ui_modules={"Entry": EntryModule},
+            xsrf_cookies=True,
+            cookie_secret="11oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo=",
+            login_url="/auth/login",
+            autoescape=None,
+        )
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+        # Have one global connection to the blog DB across all handlers
+        self.db = tornado.database.Connection(
+            host=options.mysql_host, database=options.mysql_database,
+            user=options.mysql_user, password=options.mysql_password)
+
+
+class BaseHandler(tornado.web.RequestHandler):
+    @property
+    def db(self):
+        return self.application.db
+
+    def get_current_user(self):
+        user_id = self.get_secure_cookie("user")
+        if not user_id: return None
+        return self.db.get("SELECT * FROM authors WHERE id = %s", int(user_id))
+
+
+class HomeHandler(BaseHandler):
     def get(self):
-        template_values = {}
-        path = os.path.join(os.path.dirname(__file__), 'templates', 'index.html')
-        output = template.render(path, template_values)
-        self.response.out.write(output)
-        
-class notesHandler(webapp.RequestHandler):
-    def get(self):
-        template_values = {}
-        path = os.path.join(os.path.dirname(__file__), 'templates', 'notes.html')
-        output = template.render(path, template_values)
-        self.response.out.write(output)
+        entries = self.db.query("SELECT * FROM entries ORDER BY published "
+                                "DESC LIMIT 5")
+        if not entries:
+            self.redirect("/compose")
+            return
+        self.render("index.html", entries=entries)
 
-class siteHandler(webapp.RequestHandler):
-	def get(self):
-		template_values = {}
-		path = os.path.join(os.path.dirname(__file__), 'templates', 'site.html')
-		output = template.render(path, template_values)
-		self.response.out.write(output)
+
+     
+class NotesHandler(BaseHandler):
+    def get(self):
+        self.render("notes.html")
+
+class SiteHandler(BaseHandler):
+    def get(self):
+        self.render("site.html")
     
 
-application = webapp.WSGIApplication([('/', MainPage),
-                                      ('/notes',notesHandler),
-									  ('/site',siteHandler),
-                                      ], debug=True)
+class EntryModule(tornado.web.UIModule):
+    def render(self, entry):
+        return self.render_string("modules/entry.html", entry=entry)
 
-
-settings = {
-    "blog_title": u"ZStart Page",
-    "template_path": os.path.join(os.path.dirname(__file__), "templates"),
-    "xsrf_cookies": True,
-    "autoescape": None,
-}
 
 def main():
-    run_wsgi_app(application)
+    tornado.options.parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+
 
 if __name__ == "__main__":
     main()
